e78d6bd412b78ab3250938c7698c872bc439f622
dangdinh- <dangdinh@cloud-ace.com>
2025-08-05 11:06:44 +0700

    Initial commit: ABS Technology Custom Moodle v1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# ABS Technology Custom Moodle
+
+Customized Moodle 5.0.1 for ABS Technology.
+
+## Contents:
+- \`moodle/\` - Custom Moodle source code
+- \`database/\` - Database initialization script
+
+## Version: v1.0.0
---
+++ moodle/moodledata/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = '6UqBEdi5MIge2DxnI4ExaDlY1TClg7yRlocalhost';
+$CFG->bootstraphash = '874cc714524472c55b37138c3420fa10';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
